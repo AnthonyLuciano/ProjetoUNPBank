d620ad0eef597b0e961ca43203f4385b886606d5
SidneyLcss <sidney.lucas0404@gmail.com>
2024-05-16 10:14:05 -0300

    Create BemVindo.java

---
+++ BemVindo.java
@@ -0,0 +1,11 @@
+import.java.util.Scanner;
+public class Name {
+Public static void main(String[] args) {
+Scanner scanner = new java.util.Scanner(System.in);
+System.out.println("Digite seu nome:");
+String Name = Scanner.nextLine();
+System.out.println("Bem vindo " + name + "!");
+scanner.close();
+}
+}
+//abrir aplicativo e falar Bem Vindo ao usuário.
